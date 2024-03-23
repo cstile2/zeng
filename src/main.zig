3fa624dd9424375f386774a86938bb260b80a52f
cstile2 <cstile2@illinois.edu>
2024-03-23 06:58:32 -0500

    Renamed some stuff

--- src/main.zig
+++ src/main.zig
@@ -10,7 +10,7 @@ const Engine = struct {
     const systems = @import("systems.zig");
 };
 
-pub const DataBase = struct {
+pub const GlobalData = struct {
     time: f32 = 0.0,
     active_window: *glfw.Window,
     active_camera_matrix: *[16]f32,
@@ -75,7 +75,7 @@ pub fn OnWindowResize(window: glfw.Window, width: i32, height: i32) void {
 }
 
 // Commands
-pub fn RunCommand(db: *DataBase, input_read: []const u8) void {
+pub fn RunCommand(gd: *GlobalData, input_read: []const u8) void {
     const separated: [][]u8 = Engine.SeparateText(input_read, ';');
     defer {
         for (separated) |string| {
@@ -93,20 +93,20 @@ pub fn RunCommand(db: *DataBase, input_read: []const u8) void {
         }
         if (std.mem.eql(u8, parsed[0], "import")) {
             if (parsed.len >= 2) {
-                Engine.ImportModelAsset(parsed[1], std.heap.c_allocator, db.shader_program_GPU, db.texture_GPU, &db.entity_slice);
+                Engine.ImportModelAsset(parsed[1], std.heap.c_allocator, gd.shader_program_GPU, gd.texture_GPU, &gd.entity_slice);
             } else {
                 std.debug.print("No path specified", .{});
             }
         }
         if (std.mem.eql(u8, parsed[0], "freeze") and parsed.len == 1) { // pauses everything in the game except a spectator camera
             std.debug.print("FREEZE!\n", .{});
-            db.frozen = !db.frozen;
+            gd.frozen = !gd.frozen;
         } else if (std.mem.eql(u8, parsed[0], "add")) { // adds a component to an entity
             if (parsed.len >= 3) {
                 if (std.fmt.parseInt(u32, parsed[1], 10)) |parsed_int| {
                     const index: u32 = parsed_int;
                     std.debug.print("modifying index: {any}\n", .{index});
-                    AddComponent(&db.entity_slice[index], parsed[2]) catch {
+                    AddComponent(&gd.entity_slice[index], parsed[2]) catch {
                         std.debug.print("Could not add component: '{s}'", .{parsed[2]});
                     };
                 } else |_| {
@@ -120,7 +120,7 @@ pub fn RunCommand(db: *DataBase, input_read: []const u8) void {
                 if (std.fmt.parseInt(u32, parsed[1], 10)) |parsed_int| {
                     const index: u32 = parsed_int;
                     std.debug.print("modifying index: {any}\n", .{index});
-                    RemoveComponent(&db.entity_slice[index], parsed[2]) catch {
+                    RemoveComponent(&gd.entity_slice[index], parsed[2]) catch {
                         std.debug.print("Could not remove component: '{s}'", .{parsed[2]});
                     };
                 } else |_| {
@@ -166,10 +166,10 @@ pub fn main() !void {
     c.stbi_set_flip_vertically_on_load(1);
 
     // initialize shared data
-    var db: DataBase = undefined;
+    var gd: GlobalData = undefined;
 
     // import shader from files and create a program stored in shader_program_GPU
-    db.shader_program_GPU = gl.createProgram();
+    gd.shader_program_GPU = gl.createProgram();
     {
         // get code from vertex shader file as a string (c style).
         const vert_shader_code = Engine.GetBytesFromFile("assets/shaders/basic.shader", std.heap.c_allocator);
@@ -206,13 +206,13 @@ pub fn main() !void {
         }
 
         // create shader program > attach vertex + fragment shaders
-        gl.attachShader(db.shader_program_GPU, vertex_shader_GPU);
-        gl.attachShader(db.shader_program_GPU, frag_shader_GPU);
-        gl.linkProgram(db.shader_program_GPU);
+        gl.attachShader(gd.shader_program_GPU, vertex_shader_GPU);
+        gl.attachShader(gd.shader_program_GPU, frag_shader_GPU);
+        gl.linkProgram(gd.shader_program_GPU);
     }
 
     // create a texture from file
-    db.texture_GPU = undefined;
+    gd.texture_GPU = undefined;
     {
         // load image texture via stb_image library
         var width: i32 = undefined;
@@ -222,8 +222,8 @@ pub fn main() !void {
         defer c.stbi_image_free(image_data);
 
         // create texture location > bind > set filtering > put the array data into the texture > generate mips
-        gl.genTextures(1, &db.texture_GPU);
-        gl.bindTexture(gl.TEXTURE_2D, db.texture_GPU);
+        gl.genTextures(1, &gd.texture_GPU);
+        gl.bindTexture(gl.TEXTURE_2D, gd.texture_GPU);
         defer gl.bindTexture(gl.TEXTURE_2D, 0);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
@@ -240,30 +240,30 @@ pub fn main() !void {
 
     // initialize state
     var t_pressed_last_frame: bool = false;
-    db.active_window = &window;
-    db.time = 0.0;
+    gd.active_window = &window;
+    gd.time = 0.0;
 
     // create the array of meshes to be rendered each frame
     var entity_array: [32]Engine.Entity = undefined;
-    db.entity_slice = entity_array[0..0];
+    gd.entity_slice = entity_array[0..0];
 
     // create camera entity
-    Engine.CreateEntity(&db.entity_slice, Engine.Entity{ .vao_gpu = 0, .indices_length = 0, .material = undefined, .world_matrix = Engine.identity(), .camera = Engine.Camera{ .projection_matrix = Engine.perspective_projection_matrix(1.3, 12.0 / 9.0, 0.01, 100.0) }, .component_flags = Engine.ComponentFlags{ .camera = true, .ghost = true } });
+    Engine.CreateEntity(&gd.entity_slice, Engine.Entity{ .vao_gpu = 0, .indices_length = 0, .material = undefined, .world_matrix = Engine.identity(), .camera = Engine.Camera{ .projection_matrix = Engine.perspective_projection_matrix(1.3, 12.0 / 9.0, 0.01, 100.0) }, .component_flags = Engine.ComponentFlags{ .camera = true, .ghost = true } });
     // make this camera the one to be used for rendering
-    db.active_camera_matrix = &db.entity_slice[db.entity_slice.len - 1].world_matrix;
-    db.active_camera = &db.entity_slice[db.entity_slice.len - 1].camera;
-    window.setUserPointer(&db.active_camera.projection_matrix);
+    gd.active_camera_matrix = &gd.entity_slice[gd.entity_slice.len - 1].world_matrix;
+    gd.active_camera = &gd.entity_slice[gd.entity_slice.len - 1].camera;
+    window.setUserPointer(&gd.active_camera.projection_matrix);
 
     // create system schedule
-    var systems_array: [32]*const fn (*DataBase) void = undefined;
-    var systems_slice: []*const fn (*DataBase) void = systems_array[0..0];
+    var systems_array: [32]*const fn (*GlobalData) void = undefined;
+    var systems_slice: []*const fn (*GlobalData) void = systems_array[0..0];
     Engine.systems.AddSystem(Engine.systems.SYSTEM_Input, &systems_slice);
     Engine.systems.AddSystem(Engine.systems.SYSTEM_Constant, &systems_slice);
     Engine.systems.AddSystem(Engine.systems.SYSTEM_SineMover, &systems_slice);
     Engine.systems.AddSystem(Engine.systems.SYSTEM_Ghost, &systems_slice);
     Engine.systems.AddSystem(Engine.systems.SYSTEM_CameraControls, &systems_slice);
 
-    RunCommand(&db, "import assets/blender_files/custom_export.bin");
+    RunCommand(&gd, "import assets/blender_files/custom_export.bin");
 
     // repeat until user closes the window
     while (!window.shouldClose()) {
@@ -274,22 +274,22 @@ pub fn main() !void {
         if (window.getKey(glfw.Key.t) == glfw.Action.press and !t_pressed_last_frame) {
             const input_read: []u8 = Engine.GetBytesFromFile("assets/extras/command_input.txt", std.heap.c_allocator);
             defer std.heap.c_allocator.free(input_read);
-            RunCommand(&db, input_read);
+            RunCommand(&gd, input_read);
         }
         t_pressed_last_frame = window.getKey(glfw.Key.t) == glfw.Action.press;
 
         // call all systems
         for (systems_slice) |system| {
-            if (!db.frozen or system == &Engine.systems.SYSTEM_CameraControls or system == &Engine.systems.SYSTEM_Ghost or system == &Engine.systems.SYSTEM_Input) {
-                system(&db);
+            if (!gd.frozen or system == &Engine.systems.SYSTEM_CameraControls or system == &Engine.systems.SYSTEM_Ghost or system == &Engine.systems.SYSTEM_Input) {
+                system(&gd);
             }
         }
-        Engine.systems.SYSTEM_MeshDrawer(&db); // render should be last
+        Engine.systems.SYSTEM_MeshDrawer(&gd); // render should be last
 
         // poll events
         glfw.pollEvents();
 
         // calculate frame delta
-        db.frame_delta = @as(f64, @floatFromInt(@divTrunc(std.time.nanoTimestamp() - start_frame_time, 100))) / 10000000.0;
+        gd.frame_delta = @as(f64, @floatFromInt(@divTrunc(std.time.nanoTimestamp() - start_frame_time, 100))) / 10000000.0;
     }
 }
